955f67f958857f1cbd4ea28dc3ac3d0a2ccfa388
Sergiy <sergiy@sergiy.(none)>
2015-04-29 15:35:38 +0300

    api sign-up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,8 +6,12 @@ class RegistrationsController < DeviseTokenAuth::RegistrationsController
   end
 
   protected
-
+=begin
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up).push(:registration_id, :email, :password, :password_confirmation, {:registration => []})
+    devise_parameter_sanitizer.for(:sign_up).push(:registration_id, :email, :password, :password_confirmation, :registration => {})
+  end
+=end
+  def sign_up_params
+   params.require(:user).permit(:email, :password, :password_confirmation, :registration_id, registration: [:email, :password, :password_confirmation, :registration_id])
   end
 end
