d7a6b9d298a578baa2ac926c57328c376115f2d1
Sergiy <sergiy@sergiy.(none)>
2015-04-29 16:14:12 +0300

    ppppp

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,25 @@ class RegistrationsController < DeviseTokenAuth::RegistrationsController
   #before_filter :configure_permitted_parameters
   
     def create
-      super
+      @resource            = resource_class.new(sign_up_params)
+      @resource.provider   = "email"
+
+      # honor devise configuration for case_insensitive_keys
+      if resource_class.case_insensitive_keys.include?(:email)
+        @resource.email = sign_up_params[:email].downcase
+      else
+        @resource.email = sign_up_params[:email]
+      end
+
+            @client_id = SecureRandom.urlsafe_base64(nil, false)
+            @token     = SecureRandom.urlsafe_base64(nil, false)
+
+            @resource.tokens[@client_id] = {
+              token: BCrypt::Password.create(@token),
+              expiry: (Time.now + DeviseTokenAuth.token_lifespan).to_i
+            }
+
+            @resource.save!
     end
 
   protected
