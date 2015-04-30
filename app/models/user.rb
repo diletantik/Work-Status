d379949a927cb0986b6a9af36880ffb59c266f6d
Sergiy <sergiy@sergiy.(none)>
2015-04-30 14:44:35 +0300

    ololo

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable, :encrypted_password
+          :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
