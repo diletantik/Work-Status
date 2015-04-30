4a40dbc5837be98c5f37222f4f639277c789d4b5
Sergiy <sergiy@sergiy.(none)>
2015-04-30 13:43:05 +0300

    add sign in devise functionality

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,29 @@
+class Api::UsersController < ApplicationController
+  before_filter :authenticate
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  require 'gcm'
+
+  # GET /events
+  # GET /events.json
+  def index
+    if $current_user
+      @users = User.all
+      #respond_to do |format|
+        #format.json { render :json => @events }
+      #end
+      render json: @users
+    else
+      render json: {:message => "U dont have permission to look this page"}
+    end
+  end
+
+  def sign_in
+    @password = params[:password].encrypted_password
+    @user = User.where('email = ? AND password = ?', params[:eamil], @password).first
+    if @user
+      render json: {:registration_id => @user.registration_id}
+    else
+      render json: {:message => "Wrong email or password"}
+    end
+  end
+end
