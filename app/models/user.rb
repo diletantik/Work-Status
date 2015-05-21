0bb979ea5b59516b7580eeed2fe9461762316059
Sergiy <sergiy@sergiy.(none)>
2015-05-21 15:05:39 +0300

    kkk

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,10 @@ class User < ActiveRecord::Base
   def full_name
     first_name.to_s + " " + last_name.to_s
   end
+  
+      def avatar_url
+        avatar.url(:medium)
+    end
 
   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
